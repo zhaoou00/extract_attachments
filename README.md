cee87bf0b75c4614994a4ab4011e267be68a1388
Joe Yu <cublai@yahoo.com>
2023-10-10 00:37:07 -0400

    update README because of the 10k messages limit. Create folder on the server instead to get around the issue

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # extract_attachments
+
 1) Use thunderbird to connect to your email through IMAP 
-2) Search in thunderbird all the emails from home depot
-3) create in thunderbird "Local Folders" a folder named "home depot"
-4) right click the emails from 2) and "Copy to" that "home depot"folder
+2) In Yahoo, create folder "home depot YYYY", search the emails from homedepot and move them to this folder. The reason is that yahoo cap the number of emails at 10,000. We aren't able to get old messages by simply searching locally.
+3) create in thunderbird "Local Folders" a folder named "home depot YYYY"
+4) right click the emails from 2) and "Copy to" that "home depot YYYY"folder
 5) Right click that"home depot" folder and see "properties". Find the location of the file storing all the messages.
 6) run my code pointing to that file to extract all attachments
--- extract.py
+++ extract.py
@@ -1,5 +1,5 @@
 import mailbox, pdb
-def extractattachements(message, path = r'C:\Users\cubla\Downloads\homedepot 2\ '[:-1]):
+def extractattachements(message, path = r'C:\Users\cubla\Downloads\homedepot 2021\ '[:-1]):
     if message.get_content_maintype() == 'multipart':
         for part in message.walk():
             if part.get_content_maintype() == 'multipart': continue
@@ -9,7 +9,7 @@ def extractattachements(message, path = r'C:\Users\cubla\Downloads\homedepot 2\ 
             fb = open(filename,'wb')
             fb.write(part.get_payload(decode=True))
             fb.close()
-mbox = mailbox.mbox(r'C:\Users\cubla\AppData\Roaming\Thunderbird\Profiles\7fn4g6kx.default-release\Mail\Local Folders\home depot 2')
+mbox = mailbox.mbox(r'C:\Users\cubla\AppData\Roaming\Thunderbird\Profiles\njlwv0k2.default-release\Mail\Local Folders\home depot 2021')
 for message in mbox:
     extractattachements(message)
     
